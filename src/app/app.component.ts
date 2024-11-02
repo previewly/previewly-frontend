a92fbb6122e30bb8243abab2da5f9af531cff046
wert2all <wert2all@gmail.com>
2024-11-02 10:43:00 +0200

    refactor: refactor appliction to use routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { RouterOutlet } from '@angular/router';
-import { concatLatestFrom } from '@ngrx/operators';
-import { Store } from '@ngrx/store';
-import { interval, map } from 'rxjs';
+import { Component } from '@angular/core';
 
-import { CodeContainerComponent } from './code-container/code-container.component';
-import { InputUrlComponent } from './input-url/input-url.component';
-import { PreviewContainerComponent } from './preview-container/preview-container.component';
-import { TitleComponent } from './share/content/title/title.component';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './share/footer/footer.component';
 import { LogoComponent } from './share/logo/logo.component';
-import { PreviewActions } from './store/preview/preview.actions';
-import { previewFeature } from './store/preview/preview.reducers';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [
-    RouterOutlet,
-    TitleComponent,
-    InputUrlComponent,
-    PreviewContainerComponent,
-    CodeContainerComponent,
-    LogoComponent,
-    FooterComponent,
-  ],
+  imports: [LogoComponent, FooterComponent, RouterOutlet],
 })
-export class AppComponent {
-  private readonly store = inject(Store);
-  token = this.store.selectSignal(previewFeature.selectToken);
-
-  userPreviews = this.store.selectSignal(previewFeature.selectPreviews);
-  isLoading = this.store.selectSignal(previewFeature.selectIsLoading);
-
-  constructor() {
-    interval(3000)
-      .pipe(
-        takeUntilDestroyed(),
-        concatLatestFrom(() =>
-          this.store.select(previewFeature.selectShouldUpdate)
-        ),
-        map(([, shouldUpdate]) => shouldUpdate)
-      )
-      .subscribe(urls => {
-        if (urls.length > 0) {
-          this.store.dispatch(PreviewActions.updatePreviews({ urls: urls }));
-        }
-      });
-  }
-
-  addUrl(url: string) {
-    this.store.dispatch(PreviewActions.addNewUrl({ url }));
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/index-page/index-page.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { concatLatestFrom } from '@ngrx/operators';
+import { Store } from '@ngrx/store';
+import { interval, map } from 'rxjs';
+import { CodeContainerComponent } from '../../code-container/code-container.component';
+import { InputUrlComponent } from '../../input-url/input-url.component';
+import { PreviewContainerComponent } from '../../preview-container/preview-container.component';
+import { PreviewActions } from '../../store/preview/preview.actions';
+import { previewFeature } from '../../store/preview/preview.reducers';
+
+@Component({
+  selector: 'app-index-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    InputUrlComponent,
+    PreviewContainerComponent,
+    CodeContainerComponent,
+  ],
+  templateUrl: './index-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class IndexPageComponent {
+  private readonly store = inject(Store);
+  protected readonly token = this.store.selectSignal(
+    previewFeature.selectToken
+  );
+
+  protected readonly userPreviews = this.store.selectSignal(
+    previewFeature.selectPreviews
+  );
+  protected readonly isLoading = this.store.selectSignal(
+    previewFeature.selectIsLoading
+  );
+
+  constructor() {
+    interval(3000)
+      .pipe(
+        takeUntilDestroyed(),
+        concatLatestFrom(() =>
+          this.store.select(previewFeature.selectShouldUpdate)
+        ),
+        map(([, shouldUpdate]) => shouldUpdate)
+      )
+      .subscribe(urls => {
+        if (urls.length > 0) {
+          this.store.dispatch(PreviewActions.updatePreviews({ urls: urls }));
+        }
+      });
+  }
+
+  addUrl(url: string) {
+    this.store.dispatch(PreviewActions.addNewUrl({ url }));
+  }
+}
