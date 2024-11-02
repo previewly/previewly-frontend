f1283f7e845f916875f8c2006b107e57b28247ea
wert2all <wert2all@gmail.com>
2024-11-02 11:04:22 +0200

    feat: add upload route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { UploadPageComponent } from './pages/upload-page/upload-page.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: IndexPageComponent,
-  },
+  { path: '', component: IndexPageComponent },
+  { path: 'upload', component: UploadPageComponent },
 ];
