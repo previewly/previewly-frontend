30d7962970d9e6810163a99cf60a62e5c30bd96a
wert2all <wert2all@gmail.com>
2024-11-03 12:35:36 +0200

    feat: add form for upload

---
+++ src/app/pages/upload-page/components/upload-filelist/upload-filelist.types.ts
@@ -0,0 +1,5 @@
+import { Loadable } from '../../../../app.types';
+
+export type FileItem = Loadable & {
+  name: string;
+};
--- src/app/pages/upload-page/upload-page.component.ts
+++ src/app/pages/upload-page/upload-page.component.ts
@@ -1,12 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FeatureFlagComponent } from '../../feature-flag/feature-flag.component';
+import { UploadFilelistComponent } from './components/upload-filelist/upload-filelist.component';
+import { FileItem } from './components/upload-filelist/upload-filelist.types';
+import { UploadFormHeaderComponent } from './components/upload-form-header/upload-form-header.component';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
 
 @Component({
   selector: 'app-upload-page',
   standalone: true,
-  imports: [CommonModule, FeatureFlagComponent],
+  imports: [
+    CommonModule,
+    FeatureFlagComponent,
+    UploadFormComponent,
+    UploadFormHeaderComponent,
+    UploadFilelistComponent,
+  ],
   templateUrl: './upload-page.component.html',
+  styleUrls: ['./upload-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UploadPageComponent {}
+export class UploadPageComponent {
+  protected readonly uploadFiles = signal<FileItem[]>([]);
+
+  selectedFiles(files: File[]) {
+    this.uploadFiles.set(
+      files.map(file => ({
+        name: file.name,
+        isLoading: true,
+      }))
+    );
+  }
+}
