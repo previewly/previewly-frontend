a0714365a56eb1b6118624f5c5de32cf29d395e9
wert2all <wert2all@gmail.com>
2024-11-13 11:17:32 +0200

    feat: add terms of service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { IndexPageComponent } from './pages/index-page/index-page.component';
 import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.component';
+import { TermsPageComponent } from './pages/terms-page/terms-page.component';
 import { UploadPageComponent } from './pages/upload-page/upload-page.component';
 
 export const routes: Routes = [
   { path: '', component: IndexPageComponent },
+  { path: 'terms', component: TermsPageComponent },
   { path: 'privacy', component: PrivacyPageComponent },
   { path: 'upload', component: UploadPageComponent },
 ];
