0b4a2e55feaa73fa05d2197f6cbdb2d723a5e201
wert2all <wert2all@gmail.com>
2024-11-13 11:17:32 +0200

    feat: add terms of service

---
+++ src/app/pages/terms-page/terms-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-terms-page',
+  templateUrl: './terms-page.component.html',
+  styleUrls: ['./terms-page.component.scss'],
+})
+export class TermsPageComponent {
+  protected readonly today = new Date().toISOString().slice(0, 10);
+}
