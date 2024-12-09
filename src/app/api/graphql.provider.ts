dd7ab7037b142f664608a9be2a9ecce72a3600a9
wert2all <wert2all@gmail.com>
2024-12-09 16:02:48 +0200

    feat: add upload image mutation

--- src/app/api/graphql.provider.ts
+++ src/app/api/graphql.provider.ts
@@ -1,13 +1,9 @@
-import { inject, makeEnvironmentProviders } from '@angular/core';
+import { makeEnvironmentProviders } from '@angular/core';
 import { ApolloLink, InMemoryCache } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
 import { APOLLO_OPTIONS, Apollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-
+import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
 import { environment } from '../../environments/environment';
-
-const httpLink = (httpLink = inject(HttpLink)) =>
-  httpLink.create({ uri: environment.graphqlUrl });
 
 const baseLink = () =>
   setContext(() => ({ headers: { Accept: 'charset=utf-8' } }));
@@ -18,7 +14,10 @@ export const provideApollo = () =>
     {
       provide: APOLLO_OPTIONS,
       useFactory: () => ({
-        link: ApolloLink.from([baseLink(), httpLink()]),
+        link: ApolloLink.from([
+          baseLink(),
+          createUploadLink({ uri: environment.graphqlUrl }),
+        ]),
         cache: new InMemoryCache(),
       }),
     },
