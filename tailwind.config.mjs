209f364fe4308fd32602d8b0a1f2c116d4d778df
Serhii Shvets <wert2all@gmail.com>
2025-01-30 15:47:57 +0200

    refactor: remove @apply from code

--- tailwind.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  content: ['./src/**/*.{html,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-  daisyui: { themes: ['garden', 'night'], logs: false },
-};
