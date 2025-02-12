8e8d57936ab6cd55919ac9c394ee87a11b9af175
wert2all <wert2all@gmail.com>
2025-02-12 14:33:47 +0200

    feat: add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
+import * as Sentry from '@sentry/angular';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn: environment.sentry.dsn,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
