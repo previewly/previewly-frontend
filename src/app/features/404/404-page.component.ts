a47230ce434f265a4d6c278115817c07321de2e1
wert2all <wert2all@gmail.com>
2025-02-21 11:30:42 +0200

    feat: create 404 error page

---
+++ src/app/features/404/404-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  templateUrl: './404-page.component.html',
+  styleUrls: ['./404-page.component.scss'],
+})
+export class NotFoundPageComponent {}
