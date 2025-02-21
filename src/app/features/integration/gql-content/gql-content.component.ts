5a69cb3ab02ab9503e7dcbfac79ccd14e7c75a79
wert2all <wert2all@gmail.com>
2025-02-21 13:03:55 +0200

    fix: add refresh token button for API integrations

--- src/app/features/integration/gql-content/gql-content.component.ts
+++ src/app/features/integration/gql-content/gql-content.component.ts
@@ -1,5 +1,8 @@
-import { Component, computed, input } from '@angular/core';
-
+import { Component, computed, input, output } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { phosphorArrowsClockwiseLight } from '@ng-icons/phosphor-icons/light';
+import { phosphorXCircle } from '@ng-icons/phosphor-icons/regular';
+import { Undefined } from '../../../app.types';
 import { CopyToClipboardComponent } from '../../../shared/ui/tools/copy-to-clipboard/copy-to-clipboard.component';
 import { CodeAndResultComponent } from '../code-and-result/code-and-result.component';
 
@@ -8,11 +11,18 @@ import { CodeAndResultComponent } from '../code-and-result/code-and-result.compo
   selector: 'app-gql-content',
   templateUrl: './gql-content.component.html',
   styleUrls: ['./gql-content.component.scss'],
-  imports: [CopyToClipboardComponent, CodeAndResultComponent],
+  imports: [CopyToClipboardComponent, CodeAndResultComponent, NgIconComponent],
+  viewProviders: [
+    provideIcons({ phosphorArrowsClockwiseLight, phosphorXCircle }),
+  ],
 })
 export class GqlContentComponent {
   gqlUrl = input.required<string>();
   token = input.required<string>();
+
+  error = input<string | Undefined>(null);
+
+  refreshToken = output();
 
   getAddUrlCode = computed(
     () =>
--- src/app/features/integration/shared/code-container/code-container.component.ts
+++ src/app/features/integration/shared/code-container/code-container.component.ts
@@ -5,9 +5,11 @@ import {
   computed,
   inject,
   input,
+  output,
   signal,
 } from '@angular/core';
 
+import { Undefined } from '../../../../app.types';
 import { TitleComponent } from '../../../../shared/ui/content/title/title.component';
 import { SharedLoaderComponent } from '../../../../shared/ui/tools/loader/loader.component';
 import { ApiUrlService } from '../../api-url.service';
@@ -31,8 +33,11 @@ export class CodeContainerComponent {
   private readonly apiUrlService = inject(ApiUrlService);
   protected readonly activeTab = signal(0);
 
-  token = input.required<string | undefined>();
+  token = input.required<string>();
   isLoading = input<boolean>(false);
+  error = input<string | Undefined>(null);
+
+  refreshToken = output();
 
   imgUrl = computed(() => {
     const token = this.token();
