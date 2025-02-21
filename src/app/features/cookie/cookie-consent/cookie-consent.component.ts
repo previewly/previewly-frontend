338713d3d8a6b74c6a8c8bc6cfa847f7493a8b69
wert2all <wert2all@gmail.com>
2025-02-21 14:22:19 +0200

    refactor: update routes for legal informations

--- src/app/features/cookie/cookie-consent/cookie-consent.component.ts
+++ src/app/features/cookie/cookie-consent/cookie-consent.component.ts
@@ -27,8 +27,8 @@ export class CookieConsentComponent implements AfterViewInit {
             acceptNecessaryBtn: 'Reject all',
             showPreferencesBtn: 'Manage Individual preferences',
             footer: `
-                    <a class="link" href="/cookie-policy" target="_blank">Cookie Policy</a>
-                    <a class="link" href="/privacy" target="_blank">Privacy Policy</a>
+                    <a class="link" href="/legal/cookie-policy" target="_blank">Cookie Policy</a>
+                    <a class="link" href="/legal/privacy" target="_blank">Privacy Policy</a>
                 `,
           },
           preferencesModal: {
