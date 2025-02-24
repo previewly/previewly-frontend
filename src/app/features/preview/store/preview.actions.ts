ac2e9d52e46229fa6264ca5e36d04d50bf9ea6d5
wert2all <wert2all@gmail.com>
2025-02-24 13:36:24 +0200

    feat: expose preview by service

--- src/app/features/preview/store/preview.actions.ts
+++ src/app/features/preview/store/preview.actions.ts
@@ -1,20 +1,13 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 
 import { PreviewItem } from './preview.types';
 
 export const PreviewActions = createActionGroup({
   source: 'Preview',
   events: {
-    'Add urls from local storage': props<{ urls: PreviewItem[] }>(),
-
     'Add new url': props<{ url: string }>(),
-    'Success add new url': props<{ urls: PreviewItem[] }>(),
-
-    'Empty token': emptyProps(),
-    'Empty token on adding new url': emptyProps(),
-
-    'Update previews': props<{ urls: PreviewItem[] }>(),
-    'Success update previews': props<{ urls: PreviewItem[] }>(),
+    'Success add new url': props<{ url: PreviewItem }>(),
+    'Error add new url': props<{ error: string }>(),
 
     'Remove preview': props<{ url: string }>(),
   },
--- src/app/features/preview/store/preview.reducers.ts
+++ src/app/features/preview/store/preview.reducers.ts
@@ -1,70 +1,56 @@
-import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
+import { createFeature, createReducer, on } from '@ngrx/store';
 
+import { Status } from '../../../app.types';
 import { PreviewActions } from './preview.actions';
-import { PreviewItem, PreviewState } from './preview.types';
+import { PreviewState } from './preview.types';
 
 const MAX_COUNT = 10;
-const MAX_ATTEMPTS = 10;
 
 const initialState: PreviewState = {
   previews: [],
   isLoading: true,
 };
 
-const mergePreviews = (
-  statePreviews: PreviewItem[],
-  updatePreviews: PreviewItem[]
-): PreviewItem[] => {
-  const stateKeys = statePreviews.map(url => url.url.toString());
-  const updateKeys = updatePreviews.map(url => url.url.toString());
-
-  const updatedState = statePreviews.map(url => {
-    const updateIndex = updateKeys.indexOf(url.url.toString());
-    return updateIndex != -1 ? updatePreviews[updateIndex] || url : url;
-  });
-  [...updateKeys].reverse().forEach(updatedUrl => {
-    if (stateKeys.indexOf(updatedUrl) == -1) {
-      const shouldAdd = updatePreviews.find(
-        url => url.url.toString() == updatedUrl
-      );
-      if (shouldAdd) {
-        updatedState.push(shouldAdd);
-      }
-    }
-  });
-  return updatedState.length > MAX_COUNT
-    ? updatedState.slice(updatedState.length - MAX_COUNT)
-    : updatedState;
-};
-
 export const previewFeature = createFeature({
   name: 'preview',
   reducer: createReducer(
     initialState,
+
     on(
-      PreviewActions.emptyToken,
       PreviewActions.successAddNewUrl,
+      PreviewActions.errorAddNewUrl,
       (state): PreviewState => ({ ...state, isLoading: false })
     ),
-
     on(
       PreviewActions.addNewUrl,
       (state: PreviewState): PreviewState => ({ ...state, isLoading: true })
     ),
 
-    on(
-      PreviewActions.addUrlsFromLocalStorage,
-      PreviewActions.successAddNewUrl,
-      PreviewActions.successUpdatePreviews,
-      (state, { urls }): PreviewState => ({
-        ...state,
-        previews: mergePreviews(state.previews, urls).map(url => ({
-          ...url,
-          updateAttempts: url.updateAttempts + 1,
-        })),
-      })
-    ),
+    on(PreviewActions.addNewUrl, (state, { url }) => ({
+      ...state,
+      previews: [
+        ...state.previews.filter(preview => preview.url != url),
+        {
+          url,
+          error: null,
+          data: null,
+          status: Status.LOADING,
+        },
+      ],
+    })),
 
+    on(PreviewActions.successAddNewUrl, (state, { url }): PreviewState => {
+      const previews = state.previews.map(preview =>
+        preview.url == url.url ? url : preview
+      );
+      return {
+        ...state,
+        previews:
+          previews.length > MAX_COUNT
+            ? previews.slice(previews.length - MAX_COUNT)
+            : previews,
+      };
+    }),
     on(
       PreviewActions.removePreview,
       (state, { url }): PreviewState => ({
@@ -73,12 +59,4 @@ export const previewFeature = createFeature({
       })
     )
   ),
-  extraSelectors: ({ selectPreviews }) => ({
-    selectShouldUpdate: createSelector(selectPreviews, previews =>
-      previews.filter(
-        preview =>
-          preview.status == 'pending' && preview.updateAttempts < MAX_ATTEMPTS
-      )
-    ),
-  }),
 });
