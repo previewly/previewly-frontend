5997a31052f78a21952ab215535c1a7dfc2f7606
wert2all <wert2all@gmail.com>
2025-02-26 16:48:31 +0200

    feat: add features block

---
+++ src/app/features/index-page/features/swipe-browser/swipe-browser.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swipe-browser',
+  templateUrl: './swipe-browser.component.html',
+  styleUrls: ['./swipe-browser.component.scss'],
+})
+export class SwipeBrowserComponent {}
---
+++ src/app/features/index-page/features/swipe-preview/swipe-preview.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-swipe-preview',
+  templateUrl: './swipe-preview.component.html',
+})
+export class SwipePreviewComponent {}
