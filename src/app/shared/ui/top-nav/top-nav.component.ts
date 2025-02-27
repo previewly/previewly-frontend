289718005bb2defcaeeb68162d512ea3ad526de1
Serhii Shvets <wert2all@gmail.com>
2025-02-27 12:37:34 +0200

    fix: fix nav-bar design

--- src/app/shared/ui/top-nav/top-nav.component.ts
+++ src/app/shared/ui/top-nav/top-nav.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  phosphorDotsNine,
+  phosphorList,
+} from '@ng-icons/phosphor-icons/regular';
 import { LogoComponent } from './logo/logo.component';
 
 @Component({
   standalone: true,
   selector: 'app-shared-top-nav',
   templateUrl: './top-nav.component.html',
-  imports: [LogoComponent],
+  imports: [LogoComponent, NgIconComponent],
+  viewProviders: [provideIcons({ phosphorDotsNine, phosphorList })],
 })
 export class SharedTopNavComponent {}
